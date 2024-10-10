09bf7baa64849fd7a6a70d986811b04e234e7a5e
Bisnu Ray <79582318+bisnuray@users.noreply.github.com>
2024-10-11 02:02:24 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Copyright (C) @TheSmartBisnu
+# Channel: https://t.me/itsSmartDev
+
+# Pyrogram setup
+API_ID = "123456"  # Replace this API ID with your actual API ID
+API_HASH = "123xxxxxxx"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "12345678:xxxxxxxxxxxxxxxxx"  # Replace this BOT_TOKEN
+SESSION_STRING = "xxxxxxxxxxxxxxxxxxxxxxx" # Replace this SESSION STRING with your actual SESSION_STRING
