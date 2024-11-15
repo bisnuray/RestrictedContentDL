77eb4192798240980092eeadc6da210b474d2780
xpserviceb <xpserviceb@gmail.com>
2024-11-15 14:59:36 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "123456"  # Replace this API ID with your actual API ID
-API_HASH = "123xxxxxxx"  # Replace this API HASH with your actual API HASH
-BOT_TOKEN = "12345678:xxxxxxxxxxxxxxxxx"  # Replace this BOT_TOKEN
-SESSION_STRING = "xxxxxxxxxxxxxxxxxxxxxxx" # Replace this SESSION STRING with your actual SESSION_STRING
+API_ID = "26571790"  # Replace this API ID with your actual API ID
+API_HASH = "85cf07a16f40cceb2cd7735600d59afc"  # Replace this API HASH with your actual API HASH
+BOT_TOKEN = "8012666118:AAG_5omiOeLn9q4Fd1tcUx3IbvDEK8VJf_8"  # Replace this BOT_TOKEN
+SESSION_STRING = "BAGVdA4AnXs2uWT90zZza6TjbVAvRin8-BBDOfbyVppeZixwcqQdBF7gnnyJCJSaLieJvjWofwui0Rv365XOyR9HBzDVCMRXOFnh78P1AIn6xPenZZd9bD-dh0lqNDxMuFyrK1Vt7bgtl3gCVbpAjpZIALRrvtBzPfgzdA87dIFdyTV8pRCNK5nloeuJJJu1iuVKQkmo1IVMbhqfjab-UaeFesm71c0mp-4ZmqcgQmqLTLfJKoM5J-SmJx7tb9LlwvfX8UShrQ_iuodo9g4A66wP-4k8_XO04HBDcbYjLuhxuGQTiIat2HhnGTQ7mlNd4Su1CSz_bmZkitY1v7ulqhTlVSpCLwAAAAG13UY2AA" # Replace this SESSION STRING with your actual SESSION_STRING
