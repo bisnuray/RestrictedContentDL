c485db26b3b8decf8e5580ff7ebdf588bbb2633f
Bhanwargurjar <148867343+Bhanwargurjar@users.noreply.github.com>
2024-12-28 15:26:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # Channel: https://t.me/itsSmartDev
 
 # Pyrogram setup
-API_ID = "22803742"  # Replace this API ID with your actual API ID
-API_HASH = "a35d03d8cbc717e4241202e6dd2b1998"  # Replace this API HASH with your actual API HASH
+API_ID = ""  # Replace this API ID with your actual API ID
+API_HASH = ""  # Replace this API HASH with your actual API HASH
 BOT_TOKEN = "18121818843:AAGhbZLLMHTI_kdeU7Wt-A0lJqPj7ryui5o"  # Replace this BOT_TOKEN
 SESSION_STRING = "" # Replace this SESSION STRING with your actual SESSION_STRING
