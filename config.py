fc8acf703277afc3204e37cce38f96a756fd4ae7
BioHazard786 <bzatch70@gmail.com>
2025-04-09 12:40:13 +0530

    added docker compose for deployment and minor bug fixes

--- config.py
+++ config.py
@@ -10,6 +10,17 @@ try:
 except:
     pass
 
+    if not getenv("BOT_TOKEN") or not getenv("BOT_TOKEN").count(":") == 1:
+        print("Error: BOT_TOKEN must be in format '123456:abcdefghijklmnopqrstuvwxyz'")
+        exit(1)
+
+    if (
+        not getenv("SESSION_STRING")
+        or getenv("SESSION_STRING") == "xxxxxxxxxxxxxxxxxxxxxxx"
+    ):
+        print("Error: SESSION_STRING must be set with a valid string")
+        exit(1)
+
 
 # Pyrogram setup
 class PyroConf(object):
